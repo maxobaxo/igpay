52e3cd7a1269e6b50511791ee96c1362677b6a70
Max Scher and Nathan Stewart <nathan@>
2017-06-07 16:22:47 -0700

    test single consonant logic successfully

--- js/scriptsss.js
+++ js/scriptsss.js
@@ -4,6 +4,8 @@ var pigLatin = function(word) {
 
   var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
   var vowelFirst = false;
+  var multiConsFirst = false;
+  var singleConsFirst = false;
 
   for (var i = 0; i < vowels.length; i += 1) {
     if (word[0] === vowels[i]) {
@@ -11,19 +13,18 @@ var pigLatin = function(word) {
     } else {
       for (var i = 0; i < vowels.length; i += 1) {
         for (var j = 0; j < word.length; j += 1) {
-          if (word[j] === vowels[i]) {
-            // alert(word[j] + " is the first vowel");
+          if (word[1] === vowels[i]) {
+            singleConsFirst === true;
+          } else if (word[j] === vowels[i]) {
+            multiConsFirst = true;
           }
         };
       };
     }
   };
 
-
-
-
   if (vowelFirst === true) {
-    if (vowels[5] === word[0]) {
+    if (word[0] === vowels[5]) {
       //alert("Y is not a vowel");
       var vowelYesY = word.slice(1, word.length);
       return vowelYesY + word[0] + "ay";
@@ -31,13 +32,16 @@ var pigLatin = function(word) {
       var vowelNotY = word.concat("way");
       return vowelNotY;
     }
-  } else if (vowelFirst === false) {
+  } else if (vowelFirst !== true) {
+    //logic only first letter is consonant
+    if (singleConsFirst === true) {
+      var oneConsFirst = word.slice(1, word.length);
+      return oneConsFirst + word[0] + "ay";
+    } else if (multiConsFirst === true) {
     //logic first-x letters are consonants
-
-    // var multiConsFirst =
-    //logic for first letter NOT a vowel
-    var oneConsFirst = word.slice(1, word.length);
-    return oneConsFirst + word[0] + "ay";
+    } else {
+      return "Error!";
+    }
   } else {
     return "Error!";
   }
