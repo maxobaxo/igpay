8789069db0413c2cd44a3b1283d6f280111c6920
Max Scher <maxobaxo@gmail.com>
2017-06-13 21:37:34 -0700

    add logic for words starting with multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
 // Business logic below:
-
 var pigLatin = function(word) {
 
   var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
   var vowelFirst = false;
+  var vowelSecond = false;
+  var vowelThird = false;
+  var vowelFourth = false;
 
   for (var i = 0; i < vowels.length; i += 1) {
     if (word[0] === vowels[i]) {
       vowelFirst = true;
-    } // else {
-    //   for (var i = 0; i < vowels.length; i += 1) {
-    //     for (var j = 0; j < word.length; j += 1) {
-    //       if (word[j] === vowels[i]) {
-    //         // alert(word[j] + " is the first vowel");
-    //       }
-    //     };
-    //   };
-    // }
+    } else if (word [0] !== vowels[i] && word[1] === vowels[i]) {
+      vowelSecond = true;
+    } else if (word [0] !== vowels[i] && word[1] !== vowels[i] && word[2] === vowels[i]) {
+      vowelThird = true;
+    } else if (word [0] !== vowels[i] && word[1] !== vowels[i] && word[2] !== vowels[i] && word[3] === vowels[i]) {
+      vowelFourth = true;
+    }
   };
 
-
-
+console.log("vowelFirst = " + vowelFirst);
+console.log("vowelSecond = " + vowelSecond);
+console.log("vowelThird = " + vowelThird);
+console.log("vowelFourth = " + vowelFourth);
 
   if (vowelFirst === true) {
     if (vowels[5] === word[0]) {
-      //alert("Y is not a vowel");
       var vowelYesY = word.slice(1, word.length);
       return vowelYesY + word[0] + "ay";
     } else {
@@ -33,24 +34,23 @@ var pigLatin = function(word) {
       return vowelNotY;
     }
   } else if (vowelFirst === false) {
-    //logic first-x letters are consonants
-
-    // var multiConsFirst =
+    if (vowelSecond === true) {
+      var oneConsFirst = word.slice(1, word.length);
+      return oneConsFirst + word[0] + "ay";
+    } else if (vowelThird === true) {
+      var twoConsFirst = word.slice(2, word.length);
+      return twoConsFirst + word[0] + word[1] + "ay";
+    } else if (vowelFourth === true) {
+      var threeConsFirst = word.slice(3, word.length);
+      return threeConsFirst + word[0] + word[1] + word[2] + "ay";
+    }
     //logic for first letter NOT a vowel
-    var oneConsFirst = word.slice(1, word.length);
-    return oneConsFirst + word[0] + "ay";
   } else {
     return "Error!";
   }
 };
 
-
-
-// if (/^[a-zA-Z]+$/.test(word) === false) {
-// return "Non-Letters Detected!";
-
 // UI logic below:
-
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
